a444aff23fd8642e74f9ccb13f3eb27481572630
SkylineSG <Sebastian97x@gmail.com>
2019-10-14 20:52:13 +0200

    First attempt to Heroku

---
+++ showtasks.bat
@@ -0,0 +1,21 @@
+call runcrud.bat
+if "%ERRORLEVEL%" == "0" goto runweb
+echo.
+echo RUNCRUD.BAT has errors - breaking work
+goto fail
+
+:runweb
+http://localhost:8080/crud/v1/task/getTasks
+if "%ERRORLEVEL%" == "0" goto end
+echo Cannot open page
+goto fail
+
+
+
+:fail
+echo.
+echo There were errors
+
+:end
+echo.
+echo showtasks is finished.
--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -11,9 +11,12 @@ import java.util.List;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/v1/task")
 public class TaskController {
+
+
 
     @Autowired
     private DbService service;
