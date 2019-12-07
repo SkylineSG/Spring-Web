5aa6a0f0d549600691cfe86ff766b043d5646549
SkylineSG <Sebastian97x@gmail.com>
2019-12-07 19:53:01 +0100

    23.

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -1,54 +1,13 @@
-//package com.crud.tasks.service;
-//
-//import com.crud.tasks.domain.Mail;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.mail.MailException;
-//import org.springframework.mail.SimpleMailMessage;
-//import org.springframework.mail.javamail.JavaMailSender;
-//import org.springframework.mail.javamail.MimeMessageHelper;
-//import org.springframework.mail.javamail.MimeMessagePreparator;
-//import org.springframework.stereotype.Service;
-//
-//
-//@Service
-//public class SimpleEmailService {
-//
-//    private static final Logger LOGGER = LoggerFactory.getLogger(SimpleMailMessage.class);
-//
-//    @Autowired
-//    private MailCreatorService mailCreatorService;
-//
-//    @Autowired
-//    private JavaMailSender javaMailSender;
-//
-//    public void send(final Mail mail){
-//        LOGGER.info("Email has been sent");
-//        try {
-//            SimpleMailMessage mailMessage = createMailMessage(mail);
-//            javaMailSender.send(mailMessage);
-//        }catch (MailException e){
-//            LOGGER.error("Failed to process sending", e.getMessage(),e);
-//        }
-//    }
-//
-//    private SimpleMailMessage createMailMessage(final Mail mail){
-//        SimpleMailMessage mailMessage = new SimpleMailMessage();
-//        mailMessage.setTo(mail.getMailTo());
-//        mailMessage.setSubject(mail.getSubject());
-//        mailMessage.setText(mail.getMessage());
-//        mailMessage.setCc(mail.getToCc());
-//        return mailMessage;
-//    }
-//    private MimeMessagePreparator createMimeMessage(final Mail mail) {
-//        return mimeMessage -> {
-//            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
-//            messageHelper.setTo(mail.getMailTo());
-//            messageHelper.setSubject(mail.getSubject());
-//            messageHelper.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()), true);
-//            messageHelper.setCc(mail.getToCc());
-//        };
-//    }
-//
-//}
+package com.crud.tasks.service;
+
+
+import org.springframework.stereotype.Service;
+
+import java.util.logging.Logger;
+
+
+@Service
+public class SimpleEmailService {
+
+//    private static final Logger LOGGER
+}
